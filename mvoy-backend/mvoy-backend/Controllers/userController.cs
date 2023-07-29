279f5594f50561c9a3ad8b026380663ae5753fc4
krosarioj <106118969+krosarioj@users.noreply.github.com>
2023-07-29 12:21:00 -0400

    Api Login

--- mvoy-backend/mvoy-backend/Controllers/userController.cs
+++ mvoy-backend/mvoy-backend/Controllers/userController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using mvoy.core.Enums;
 using mvoy.core.Interface;
 using mvoy.core.Models;
 using mvoy.data.DTOs;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Xml.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -90,5 +95,71 @@ namespace mvoy_backend.Controllers
         {
             return await _userService.DeleteUser(id);
         }
+
+        //Login
+
+        public IConfiguration _Configuration;
+        public userControllers(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public dynamic Login([FromBody] object optData)
+        {
+            var data = JsonConvert.DeserializeObject<dynamic>(optData.ToString());
+
+            string user = data.usuario.ToString();
+            string password = data.password.ToString();
+
+            usuario usuario = usuario.DB().Where(x => x.user == user && x.password == password).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return new
+                {
+                    success = false,
+                    message = "Credenciales incorrectas",
+                    Result = ""
+                };
+            }
+            var jwt = _Configuration.GetSection("Jwt").Get<Jwt>();
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, jwt.Subject),
+                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                  new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                  new Claim("id", usuario.idUsuario),
+                  new Claim("usuario", usuario.user)
+
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+            var signin = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                jwt.Issuer,
+                jwt.Audience,
+                claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: signin
+
+               );
+            return new
+            {
+                success = true,
+                message = "Exito",
+                result = new JwtSecurityTokenHandler().WriteToken(token)
+            };
+        }
     }
+
+
+
+
+
+
+
+
+}
 }
--- mvoy-backend/mvoy.core/mvoy.core/Models/LoginModel.cs
+++ mvoy-backend/mvoy.core/mvoy.core/Models/LoginModel.cs
@@ -4,9 +4,15 @@ namespace mvoy_backend.Web.Models
 {
     public class LoginModel
     {
-        [JsonProperty("userName")]
-        public string UserName { get; set; }
+        [JsonProperty("idUsuario")]
+        public string idUsuario { get; set; }
+
+        [JsonProperty("User")]
+        public string User { get; set; }
+
         [JsonProperty("password")]
-        public string Password { get; set; }
+        public string password { get; set; }
+        [JsonProperty("rol")]
+        public string roll { get; set; }
     }
 }
